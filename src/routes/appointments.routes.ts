a2648e799344aa93a2cd85893986129b3e6fa4c0
luis-jr-zembruski <luis.jr.zembruski@icloud.com>
2020-11-10 16:12:36 -0300

    trabalhando com dados

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,38 +1,36 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
-const appointments: Appointment[] = [];
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
 
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
   );
 
-  if(findAppointmentInSameDate) {
+  if (findAppointmentInSameDate) {
     return response
       .status(400)
-      .json({ message: 'This appointment is already booked'});
+      .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = {
-    id: uuid(),
+  const appointment = appointmentsRepository.create({
     provider,
     date: parsedDate,
-  }
-
-  appointments.push(appointment);
+  });
 
   return response.json(appointment);
 });
